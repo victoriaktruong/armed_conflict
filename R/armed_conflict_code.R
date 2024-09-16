9b9083a96ad3bb542ec3e95c6af8ea64cc7f92aa
victoriaktruong <victoria9849@gmail.com>
2024-09-16 12:12:19 -0400

    Update armed_conflict_code.R

--- R/armed_conflict_code.R
+++ R/armed_conflict_code.R
@@ -10,6 +10,3 @@ data_long <- pivot_longer(data_wide, X2000:X2019, names_to = "Year") %>%
                           mutate(Year=as.integer(gsub("X", "",Year))) %>% rename(MatMor=value)
 
                                                                                   
-                                                                                 
-#write.csv(cleandat, file "data\maternalmortality_clean.csv", row.names=FALSE)
-
