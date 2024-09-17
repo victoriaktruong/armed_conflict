0c50072d7c0990a59f46ca6f0b573938b2049966
victoriaktruong <victoria9849@gmail.com>
2024-09-16 21:32:37 -0400

    Initial commit

---
+++ R/armed_disaster.R
@@ -0,0 +1,21 @@
+library(here)
+library(tidyverse)
+
+rawdata <- read.csv(here("original","disaster.csv"), header=TRUE)
+
+
+data <- rawdata %>% filter(Year %in% c(2000:2019))%>%
+                    filter(Disaster.Type %in% c("Earthquake","Drought")) %>%
+                    select(Year,ISO,Disaster.Type)                   
+ 
+subset_data <- data %>%
+  mutate(drought = ifelse(Disaster.Type == "Drought", 1, 0),
+         earthquake = ifelse(Disaster.Type == "Earthquake", 1, 0))
+
+summary_data <- subset_data %>%
+  group_by(Year, ISO) %>%
+  summarize(
+    drought = max(drought),       
+    earthquake = max(earthquake)  
+  ) %>%
+  ungroup() 
