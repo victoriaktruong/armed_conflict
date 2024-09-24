302d274f7f57f1ff0915d13873a81e330066ae6f
victoriaktruong <victoria9849@gmail.com>
2024-09-23 23:35:58 -0400

    fix syntax errors

--- R/conflict_data_script.R
+++ R/conflict_data_script.R
@@ -3,6 +3,7 @@ conflict_data <- read.csv(here("original","conflictdata.csv"),header=TRUE)
 conflict_data <- conflict_data %>%
   group_by(year,ISO) %>%
   summarize(conflict = sum(best))
+
 conflict_data$conflict <- ifelse(conflict_data$conflict >= 25,1,0)
 conflict_data$year <- conflict_data$year + 1 # Accounting for the lag by a year
 conflict_updated <- conflict_data
