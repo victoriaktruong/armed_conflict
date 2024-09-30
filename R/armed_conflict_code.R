a47be5bbe9022ea736db0b024214383f3e9762f7
victoriaktruong <victoria9849@gmail.com>
2024-09-30 12:03:30 -0400

    add in totdeath variable

--- R/armed_conflict_code.R
+++ R/armed_conflict_code.R
@@ -3,42 +3,43 @@ library(tidyverse)
 
 
 # Read the mortality datasets
-
 matmor <- read.csv(here("original","maternalmortality.csv"),header=TRUE)
 infmor <- read.csv(here("original","infantmortality.csv"),header=TRUE)
 neomor <- read.csv(here("original","neonatalmortality.csv"),header=TRUE)
 und5mor <- read.csv(here("original","under5mortality.csv"),header=TRUE)
 
 
-# Data cleaning function
-
-clean <- function(data, name){
-  data_subset <- data %>% 
-    select(Country.Name, X2000:X2019)
-  data_long <- data_subset %>%
-    pivot_longer(cols = starts_with("X"),names_to="year",values_to = name)
-  data_long <- data_long %>%
-    mutate(year = as.numeric(sub("X", "", year)))
-  return(data_long)
+clean <- function(dataname, varname){
+  dataname  %>% 
+    dplyr::select(Country.Name, X2000:X2019)  %>% 
+    pivot_longer(cols = starts_with("X"),
+                 names_to = "year",
+                 names_prefix = "X",
+                 values_to = varname)  %>% 
+    mutate(year = as.numeric(year))  %>% 
+    arrange(Country.Name, year)
 }
 
-# Apply the function to all existing datasets to create new datasets
-clean_matmor <- clean(matmor,"MatMor")
-clean_infmor <- clean(matmor,"InfMor")
-clean_neomor <- clean(matmor,"NeoMor")
-clean_und5mor <- clean(matmor,"Und5Mor")
+matmor <- clean(matmor, "matmor")
+infmor <- clean(infmor, "infmor")
+neomor <- clean(neomor, "neomor")
+und5mor <- clean(und5mor, "und5mor")
 
-# Merge the cleaned datasets using reduce and full_join
-merged_data <- reduce(list(clean_matmor, clean_infmor, clean_neomor, clean_und5mor),
-                      full_join, by = c("Country.Name", "year"))
 
-# Add ISO-3 to the data
+clean_list <- list(matmor, infmor, neomor, und5mor)
+
+
+clean_list %>% reduce(full_join, by = c('Country.Name', 'year')) -> merged_clean
+
+
 library(countrycode)
-merged_data$ISO <- countrycode(merged_data$Country.Name,origin = "country.name",destination = "iso3c")
-merged_data <- select(merged_data, -Country.Name)
 
+merged_clean$ISO <- countrycode(merged_clean$Country.Name, 
+                          origin = "country.name", 
+                          destination = "iso3c")
+merged_clean <- select(merged_clean, -Country.Name)
 
-
+merged_clean
 
 
 
--- R/conflict_data_script.R
+++ R/conflict_data_script.R
@@ -1,10 +1,12 @@
 conflict_data <- read.csv(here("original","conflictdata.csv"),header=TRUE)
 
-conflict_data <- conflict_data %>%
-  group_by(year,ISO) %>%
-  summarize(conflict = sum(best))
+conflict_data %>%
+  group_by(ISO, year)  %>%
+  summarise(totdeath = sum(best))  %>%
+  mutate(conflict = ifelse(totdeath < 25, 0, 1))  %>%
+  ungroup()  %>%
+  mutate(year = year + 1) -> conflict_updated # Accounting for the lag by a year
 
-conflict_data$conflict <- ifelse(conflict_data$conflict >= 25,1,0)
-conflict_data$year <- conflict_data$year + 1 # Accounting for the lag by a year
-conflict_updated <- conflict_data
+conflict_updated
+
 
